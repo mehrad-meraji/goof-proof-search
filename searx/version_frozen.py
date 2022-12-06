d68cc71604813245a6a0b2e20332dfb10285fe4c
Mehrad Meraji <contact@mehrad.dev>
2022-12-06 12:02:47 -0500

    update theme

---
+++ searx/version_frozen.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# this file is generated automatically by searx/version.py
+
+VERSION_STRING = "2022.12.05-5a712686-dirty"
+VERSION_TAG = "2022.12.05-5a712686"
+GIT_URL = "https://github.com/searxng/searxng"
+GIT_BRANCH = "master"
